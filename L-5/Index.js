d2b0f3c7b3a6a606d56f3f0b897d2dc9ebe449cb
VishalChavda06 <vc229067@gmail.com>
2025-08-07 09:39:34 +0530

    delete-function-create

--- L-5/Index.js
+++ L-5/Index.js
@@ -14,40 +14,54 @@ app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // students array to store student data
 let Students = []
 
-app.get('/' , (req , res)=>{
-    return res.render('index')
+app.get('/', (req, res) => {
+  return res.render('index')
 })
 
-app.get('/home' , (req , res)=>{
-    return res.render('home')
+app.get('/home', (req, res) => {
+  return res.render('home')
 })
 
-app.get('/form' , (req , res)=>{
-    return res.render('form', { Students: Students })
+app.get('/form', (req, res) => {
+  return res.render('form', { Students: Students })
 })
 
 //post request to add student
 app.post('/insetStudent', (req, res) => {
-    let id = Students.length + 1;
-    let name = req.body.name;
-    let age = req.body.age;
-    let email = req.body.email;
+  let id = Students.length + 1;
+  let name = req.body.name;
+  let age = req.body.age;
+  let email = req.body.email;
 
-    let student = {
-        id: id,
-        name: name,
-        age: age,
-        email: email
-    };
-    Students.push(student);
+  let student = {
+    id: id,
+    name: name,
+    age: age,
+    email: email
+  };
+  Students.push(student);
+  console.log(Students);
+  console.log(" 🟢 Stundet are successfully added..! ");
 
-    console.log(" 🟢 Stundet are successfully added..! ", Students);
-     return res.redirect('/form')
+  return res.redirect('/form')
 })
 
+// delete student by id
+app.get('/deleteStundet', (req, res) => {
+  let studentId = req.query.id;
+
+  let newDelete = Students.filter((student) => {
+    return student.id != studentId;
+  })
+
+  Students = newDelete;
+  console.log(" 🔴 Student deleted successfully!");
+  console.log(Students);
+  return res.redirect('/form');
+})
